7b7eb5c55a49808ef37713b1be27335db0673d8a
Will Dixon <dixon.will1@gmail.com>
2020-12-10 17:52:36 +0000

    POST params'

--- app.rb
+++ app.rb
@@ -2,14 +2,22 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do 
     erb :index
   end
 
   post '/fighters' do
     print params
-    @fighter_1 = params[:player_1]
-    @fighter_2 = params[:player_2]
+    session[:fighter_1] = params[:player_1]
+    session[:fighter_2] = params[:player_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @fighter_1 = session[:fighter_1]
+    @fighter_2 = session[:fighter_2]
     erb :play
   end
 
